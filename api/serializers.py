ddeb37d486783bac770b76891906164fc7ca6585
gohar <gohar262@gmail.com>
2025-07-22 22:57:46 +0100

    added validation in serials

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Book
+from datetime import date
 
 
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = "__all__"
+
+    # Field-level validation
+    def validate_isbn(self, value):
+        if len(value) != 13:
+            raise serializers.ValidationError("ISBN must be 13 characters long.")
+        if not value.isdigit():
+            raise serializers.ValidationError("ISBN must contain only digits.")
+        return value
+
+    # Object-level validation
+    def validate(self, data):
+        if data["published_date"] > date.today():
+            raise serializers.ValidationError(
+                {"published_date": "Published date cannot be in the future."}
+            )
+        return data
